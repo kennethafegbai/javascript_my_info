d971de8c884469ad122f1642deefe4182930d515
Kenneth Afegbai <65070992+kennethafegbai@users.noreply.github.com>
2022-06-24 13:01:43 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const name = "Kenneth Afegbai Akhonokhue";
 const height = "1.89M";
 const country = "Nigeria";
-const details= "Name: " + name + "\n" + "Height: " + height + "\n" + "Country: " + country
+const details= "Name: " + name + "\n" + "Height: " + height + "\n" + "Country: " + country;
 console.log(details)
